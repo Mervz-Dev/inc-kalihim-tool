b19be77134372da291897c652e05415794ddba30
Mervin Narvaez <mervinnarvaez.dev@gmail.com>
2025-11-02 11:41:44 +0800

    add buffer on metro config

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,21 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 
+// Get default Expo Metro config
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: "./global.css" });
+// Add support for NativeWind
+const metroConfig = withNativeWind(config, { input: "./global.css" });
+
+// Update resolver to handle .xlsx and polyfill buffer
+metroConfig.resolver = {
+  ...metroConfig.resolver,
+  extraNodeModules: {
+    buffer: require.resolve("buffer/"), // optional, needed for ExcelJS
+    ...metroConfig.resolver.extraNodeModules,
+  },
+  assetExts: [...metroConfig.resolver.assetExts, "xlsx"], // include .xlsx as asset
+  sourceExts: [...metroConfig.resolver.sourceExts, "cjs"], // optional, for Node modules
+};
+
+module.exports = metroConfig;
